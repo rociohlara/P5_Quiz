84086ad2e826bbb584e02c7b5dfa53a9809c876e
Rocio <36423826+rociohlara@users.noreply.github.com>
2018-04-30 12:10:02 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,4 @@ app.use(quizzes);
 router.get(GET/credits);
 router.get(GET/quizzes);
 
+//Añadir además a la vista views/index.ejs dos hiperenlaces (créditos y quizzes) que permitan acceder a las nuevas vistas haciendo click en ellos.
