ba88352a5f5be90003621376f7858ba4ab511d5c
Rocio <36423826+rociohlara@users.noreply.github.com>
2018-04-30 11:55:52 +0200

    Update credits.js

--- views/credits.js
+++ views/credits.js
@@ -1,1 +1,25 @@
 //Añadir la vista ejs de credits en el fichero views/credits.ejs.
+var express = require ('express');
+var app = express();
+app.use (express.static('public'));
+app.listen (3000);
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>CORE</title>
+</head>
+
+<body>
+
+<h1> Rocio</h1>
+
+<p>
+  <img src='images/6.jpeg'>
+</p>
+ 
+
+
+</body>
+</html>
