8e323e00741be7fc59079b153fd86b70632302ba
Rocio <36423826+rociohlara@users.noreply.github.com>
2018-05-10 11:46:54 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
 var router = express.Router();
+var express = require('express');
 
-/* GET home page. */
+const sequelize = require('../models/index.js');
+
+//gert home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/credits', function(req, res, next) {
+  res.render('credits', { title: 'ROCIO' });
+});
+
+router.get('/quizzes', function(req, res, next) {
+  sequelize.models.quiz.findAll()
+	.then(quiz => {
+		res.render ('quizzes', {array: quiz});
+
+	})
+	.catch(error => next(error));
 });
 
 module.exports = router;
