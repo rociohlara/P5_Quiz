6d44bcdd95f1b29d2da5e6650043daeb6b7bcc4e
Rocio <36423826+rociohlara@users.noreply.github.com>
2018-05-10 11:40:46 +0200

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,42 @@
-var express = require('express');
-var router = express.Router();
+const Sequelize = require('sequelize');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const options = {	logging: false, operatorsAliases: false};
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", options);
+
+const quiz = 
+      //modelo de datos
+sequelize.define(	
+	'quiz',
+	{ question: {
+		type: Sequelize.STRING,
+		unique: { msg: "Esta pregunta ya está registrada"},
+		validate: {notEmpty: {msg: "Asegurese de que la pregunta no está vacía"}}
+	},
+	  answer:{
+	  	type: Sequelize.STRING,
+	  	validate: {notEmpty: {msg: "Asegurese de que la pregunta no está vacía"}}
+	  }
 });
 
-module.exports = router;
+//para la base de datos, si no está creada la inicializo asi
+
+sequelize.sync().then(() => sequelize.models.quiz.count()).then((count) => {	
+	if (!count){
+		return sequelize.models.quiz.bulkCreate([
+				{ question: "¿capital de Italia?", answer: "Roma" },
+				{ question: "¿capital de Francia?", answer: "Paris" },
+				{ question: "¿capital de España?", answer: "Madrid" },
+				{ question: "¿capital de Portugal?", answer: "Lisboa" }
+      	{ question: "¿capital de Arabia Saudita?", answer: "Riad" },
+				{ question: "¿capital de Portugal?", answer: "Lisboa" }
+      	{ question: "¿capital de Austria?", answer: "Viena" },
+				{ question: "¿capital de Brasil?", answer: "Brasilia" }ç
+      
+				]);
+			}
+})
+.catch( err => {
+	console.log(err);
+});
+
+module.exports = sequelize;
